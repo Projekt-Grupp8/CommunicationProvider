7364b305ef78034c0edb175d13f6a6af1c667e3b
Henrik <staranderh@gmail.com>
2024-10-28 11:10:08 +0100

    Init

---
+++ VerificationProvider/Data/Contexts/DatabaseContexts.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VerificationProvider.Data.Entities;
+
+namespace VerificationProvider.Data.Contexts
+{
+    public class DatabaseContexts(DbContextOptions<DatabaseContexts> options) : DbContext(options)
+    {
+        public DbSet<VerificationRequestEntity> VerificationRequests { get; set; }
+    }
+}
