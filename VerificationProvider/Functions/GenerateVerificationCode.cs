567e06bf18bc96501f6db80f6ae14946dd636899
Henrik <staranderh@gmail.com>
2024-10-29 14:28:12 +0100

    Klar med GenerateVerificationCode

--- VerificationProvider/Functions/GenerateVerificationCode.cs
+++ VerificationProvider/Functions/GenerateVerificationCode.cs
@@ -1,26 +1,63 @@
-using System;
-using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VerificationProvider.Data.Contexts;
+using VerificationProvider.Models;
+using VerificationProvider.Services;
 
 namespace VerificationProvider.Functions
 {
-    public class GenerateVerificationCode
+    public class GenerateVerificationCode(ILogger<GenerateVerificationCode> logger, IVerificationService service)
     {
-        private readonly ILogger<GenerateVerificationCode> _logger;
+        private readonly ILogger<GenerateVerificationCode> _logger = logger;
+        private readonly IVerificationService _verificationService = service;
         
 
-        public GenerateVerificationCode(ILogger<GenerateVerificationCode> logger)
-        {
-            _logger = logger;
-        }
-        
         [Function(nameof(GenerateVerificationCode))]
         [ServiceBusOutput("email_request", Connection = "ServiceBusConnection")]
-        public async Task Run([ServiceBusTrigger("verification_request", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message,ServiceBusMessageActions messageActions)
+        public async Task<string> Run([ServiceBusTrigger("verification_request", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message,ServiceBusMessageActions messageActions)
         {
-            
+            try
+            {
+                var VerificationRequest = _verificationService.UnPackVerificationRequest(message);
+                if (VerificationRequest != null)
+                {
+                    var Code = _verificationService.GenerateCode();
+                    
+                    if(Code != null && !string.IsNullOrEmpty(Code))
+                    {
+                        var result = await _verificationService.SaveVerificationRequest(VerificationRequest, Code);
+                        if (result)
+                        {
+                            var emailRequest = _verificationService.GenerateEmailRequest(VerificationRequest.Email, Code);
+                            if (emailRequest != null)
+                            {
+                                var payLoad = _verificationService.GenerateServiceBusEmailRequest(emailRequest);
+                                if (!string.IsNullOrEmpty(payLoad))
+                                {
+                                    await messageActions.CompleteMessageAsync(message);
+                                    return payLoad;
+                                }
+                            }
+                        }
+
+                       
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"ERROR: GenerateVerificationCode.Run :: {ex.Message}");
+            }
+
+            return null!;
         }
+
+       
+
+
     }
 }
